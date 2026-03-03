3272e6933ff1cf549a599c27075c23a94b200729
Ind1211 <indrasen1211@gmail.com>
2026-03-03 14:43:47 +0530

    updated backend

--- main.tf
+++ main.tf
@@ -2,146 +2,22 @@ terraform {
   required_version = ">= 1.3.0"
 
   backend "remote" {
-    organization = "TerraformCloud1211"  # Replace with your Terraform Cloud organization name
+    organization = "TerraformCloud1211"
 
     workspaces {
-      name = "azureinfrabuild"  # Replace with the name of the workspace in Terraform Cloud
+      name = "azureinfrabuild"
     }
   }
-}
 
-required_providers {
-  azurerm = {
-    source  = "hashicorp/azurerm"
-    version = "~> 3.0"
-  }
-  tls = {
-    source  = "hashicorp/tls"
-    version = "~> 4.0"
-  }
-}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
 
-provider "azurerm" {
-  features {}
-}
-
-# -----------------------
-# Resource Group
-# -----------------------
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-# -----------------------
-# Networking
-# -----------------------
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.vm_name}-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "${var.vm_name}-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# -----------------------
-# Network Security Group (NSG)
-# -----------------------
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "${var.vm_name}-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "Allow-SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = azurerm_subnet.subnet.address_prefixes[0]  # allow all private IPs in subnet
-    destination_address_prefix = "*"
-  }
-}
-
-# -----------------------
-# Network Interface
-# -----------------------
-
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = null  # no public IP
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "assoc" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-# -----------------------
-# SSH Key Generation (TLS)
-# -----------------------
-
-resource "tls_private_key" "ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-# Optional: Output the private key so you can SSH later
-output "private_key_pem" {
-  value     = tls_private_key.ssh_key.private_key_pem
-  sensitive = true
-}
-
-# -----------------------
-# Linux VM
-# -----------------------
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = var.vm_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = var.vm_size
-  admin_username      = var.admin_username
-
-  network_interface_ids = [
-    azurerm_network_interface.nic.id,
-  ]
-
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = tls_private_key.ssh_key.public_key_openssh
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
