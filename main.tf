4ec2f459fbf9a83b25288b5631ac29c838aae37c
Ind1211 <indrasen1211@gmail.com>
2026-03-03 13:37:35 +0530

    updated backend

--- main.tf
+++ main.tf
@@ -1,22 +1,23 @@
 terraform {
   required_version = ">= 1.3.0"
 
- # cloud {
-  #  organization = "TerraformCloud1211"
+  backend "remote" {
+    organization = "TerraformCloud1211"  # Replace with your Terraform Cloud organization name
 
-   # workspaces {
-    #  name = "azureinfrabuild"
-    #}
-  #}
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+    workspaces {
+      name = "azureinfrabuild"  # Replace with the name of the workspace in Terraform Cloud
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
+  }
+}
+
+required_providers {
+  azurerm = {
+    source  = "hashicorp/azurerm"
+    version = "~> 3.0"
+  }
+  tls = {
+    source  = "hashicorp/tls"
+    version = "~> 4.0"
   }
 }
 
