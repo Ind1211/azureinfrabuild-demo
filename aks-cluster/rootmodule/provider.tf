9bd73180279fb2f8ce0a8f7da1860d669d47de98
Ind1211 <indrasen1211@gmail.com>
2026-03-11 22:37:02 +0530

    updated

--- aks-cluster/rootmodule/provider.tf
+++ aks-cluster/rootmodule/provider.tf
@@ -1,3 +1,12 @@
 provider "azurerm" {
   features {}
 }
+module "aks" {
+  source = "./modules/aks"
+    ARM_CLIENT_ID = var.ARM_CLIENT_ID
+    ARM_CLIENT_SECRET = var.ARM_CLIENT_SECRET
+    ARM_SUBSCRIPTION_ID = var.ARM_SUBSCRIPTION_ID
+    ARM_TENANT_ID = var.ARM_TENANT_ID
+    azurevm_rg = var.azurevm_rg
+  
+}
